39f23268d87c0e36e642f051b93215a6b7d2b172
HeshamAbdelalem <hesham.abdelalem1@gmail.com>
2018-07-10 21:51:08 +0200

    feat: Working with fetch and api's

--- lesson-2-async-w-jQuery/app.js
+++ lesson-2-async-w-jQuery/app.js
@@ -1,5 +1,5 @@
 /* eslint-env jquery */
-
+/* jshint esversion: 6 */
 (function () {
     const form = document.querySelector('#search-form');
     const searchField = document.querySelector('#search-keyword');
@@ -10,5 +10,50 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+
+        fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`, {
+            headers: {
+                Authorization: 'Client-ID 001da43ffec0dc5d910e5c2a13e343c8a18a928017268d0df7a757d711ff1197'
+            }
+        }).then((response) => {return response.json();})
+          .then(addImage);
+        /*
+        $.ajax({
+            url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+            headers: {
+                Authorization: 'Client-ID 001da43ffec0dc5d910e5c2a13e343c8a18a928017268d0df7a757d711ff1197'
+            }
+        }).done(addImage);
+        */
+        /*
+        $.ajax({
+            url: `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=81a4b92e766f4125a238ff7c874b81ac`
+        }).done(addArticles);
+        */
+       fetch(`http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=81a4b92e766f4125a238ff7c874b81ac`)
+       .then(response => response.json())
+       .then(addArticles);
     });
+
+    
+    function addImage(images) {
+        const firstImage = images.results[0];
+
+        responseContainer.insertAdjacentHTML('afterbegin', `<figure>
+                <img src="${firstImage.urls.small}" alt="${searchedForText}">
+                <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+            </figure>`
+        );
+    }
+
+    function addArticles(data) {
+        const articlesInHTML = data.response.docs.map(article => `<li>
+                <h2><a href="${article.web_url}">${article.headline.main}</a><h2>
+                <p>${article.snippet}</p>
+            </li>`
+        ).join('');
+
+        responseContainer.insertAdjacentHTML('beforeend', `<ul>${articlesInHTML}</ul>`);
+    }
 })();
